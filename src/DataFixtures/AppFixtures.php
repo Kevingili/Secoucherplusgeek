8c5243127cc1014cd4a642123f44c5f9ff54d6f1
Kevin <kev_kevin@hotmail.fr>
2019-05-30 20:19:56 +0200

    add user entity / login

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail("toto@toto.fr");
+        $user->setPassword($this->encoder->encodePassword($user, 'password'));
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
